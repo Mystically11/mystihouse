47b68ec8cda5b0e7d467178308c4c3cfd8b8a879
Mystically <100186990+Mystically11@users.noreply.github.com>
2024-07-26 12:29:26 +0200

    super booting 😊🦈❎🎣🍥🐡🔊🔊🔊🔊🔊

--- scripts/scrapeGithubData.js
+++ scripts/scrapeGithubData.js
@@ -1,46 +1,28 @@
 function getRT(date) {
-    const now = new Date();
-    const convert = Math.floor((now - date) / 1000);
-    const rtf = new Intl.RelativeTimeFormat('en', {numeric: 'auto'});
-
-    let i; let u;
-
-    switch (true) {
-        case (i = Math.floor(convert / 31536000)) >= 1:
-            u = 'year';
-            break;
-        case (i = Math.floor(convert / 2592000)) >= 1:
-            u = 'month';
-            break;
-        case (i = Math.floor(convert / 86400)) >= 1:
-            u = 'day';
-            break;
-        case (i = Math.floor(convert / 3600)) >= 1:
-            u = 'hour';
-            break;
-        case (i = Math.floor(convert / 60)) >= 1:
-            u = 'minute';
-            break;
-        default:
-            i = seconds;
-            u = 'second';
-    }
-    
-    return rtf.format(-i, u);
+    return date.toDateString();
 }
 
-async function getRepoData() {
+async function getRepoCommits() {
     try {
-        const commitData= await fetch('https://api.github.com/repos/mystically11/mystihouse/commits');
+        const commitData = await fetch('https://api.github.com/repos/mystically11/mystihouse/commits');
         const commits = await commitData.json();
         const lastUpdate = new Date(commits[0].commit.author.date);
+        const numberOfCommits = commits.length;
 
-        document.getElementById('last-updated-text').textContent = `last update: ${getRT(lastUpdate)}`;
+        document.querySelectorAll('[data-commit-count]').forEach(element => {
+            element.textContent += `.${numberOfCommits}`;
+        });
+
+        document.querySelectorAll('[data-last-update]').forEach(element => {
+            element.textContent = `Last update: ${getRT(lastUpdate)}`;
+        });
     } catch (error) {
-        document.getElementById('last-updated-text').textContent = `last update: i dont know`;
+        document.querySelectorAll('[data-last-update]').forEach(element => {
+            element.textContent = `Last update: I don't know`;
+        });
     }
 }
 
 window.onload = () => {
-  getRepoData();
+    getRepoCommits();
 };
