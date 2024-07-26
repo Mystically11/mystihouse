3430f324a9a0dd05dfb7541f7ed003aeacda4918
Mystically <100186990+Mystically11@users.noreply.github.com>
2024-07-26 15:10:30 +0200

    github data code Fix? €£Ö‱

--- scripts/scrapeGithubData.js
+++ scripts/scrapeGithubData.js
@@ -1,5 +1,32 @@
 function getRT(date) {
-    return date.toDateString();
+    const now = new Date();
+    const convert = Math.floor((now - date) / 1000);
+    const rtf = new Intl.RelativeTimeFormat('en', {numeric: 'auto'});
+
+    let i; let u;
+
+    switch (true) {
+        case (i = Math.floor(convert / 31536000)) >= 1:
+            u = 'year';
+            break;
+        case (i = Math.floor(convert / 2592000)) >= 1:
+            u = 'month';
+            break;
+        case (i = Math.floor(convert / 86400)) >= 1:
+            u = 'day';
+            break;
+        case (i = Math.floor(convert / 3600)) >= 1:
+            u = 'hour';
+            break;
+        case (i = Math.floor(convert / 60)) >= 1:
+            u = 'minute';
+            break;
+        default:
+            i = seconds;
+            u = 'second';
+    }
+    
+    return rtf.format(-i, u);
 }
 
 async function getRepoCommits() {
@@ -14,11 +41,11 @@ async function getRepoCommits() {
         });
 
         document.querySelectorAll('[data-last-update]').forEach(element => {
-            element.textContent = `Last update: ${getRT(lastUpdate)}`;
+            element.textContent += `${getRT(lastUpdate)}`;
         });
     } catch (error) {
         document.querySelectorAll('[data-last-update]').forEach(element => {
-            element.textContent = `Last update: I don't know`;
+            element.textContent += `i don't know`;
         });
     }
 }
