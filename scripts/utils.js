4f86c7c0eb2b6632d275d9ee1179233a51a6254f
Mystically <100186990+Mystically11@users.noreply.github.com>
2024-07-30 09:38:25 +0200

    #new : Learn More read

--- scripts/utils.js
+++ scripts/utils.js
@@ -4,4 +4,34 @@ function copyToClipboard(text) {
     }).catch(function(error) {
       console.error('not copied Sad face, error: ', error);
     });
-  }
+}
+
+function getBirthday() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const birthday = new Date(year, 7, 31);
+    const birthdayStatic = new Date('2008-08-31');
+    
+    let targetDate;
+
+    if (today > birthday) {
+        targetDate = new Date(year + 1, 7, 31);
+    } else {
+        targetDate = birthday;
+    }
+
+    const diff = targetDate - today;
+    const daysUntil = Math.ceil(diff / (1000 * 60 * 60 * 24));
+
+    const oldFreak = (birthday - birthdayStatic) / (1000 * 60 * 60 * 24 * 365.25);
+
+    switch (daysUntil) {
+        case 0 | 365:
+            return `IT'S MY BIRTHDAY! wow i am ${Math.floor(oldFreak + 1)} now`; 
+        case 1:
+            return `wow that is in ${daysUntil} day..`;
+        default:
+            return `i Will be ${Math.floor(oldFreak + 1)} in ${daysUntil} days`;
+    }
+}
+
