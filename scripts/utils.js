568f7aab175e58ef78518f667b76018bb05854dd
Mystically <100186990+Mystically11@users.noreply.github.com>
2025-02-02 21:15:00 +0100

    fix getBirthday (UNGODLY CODE I HATE JS)

--- scripts/utils.js
+++ scripts/utils.js
@@ -9,20 +9,15 @@ function copyToClipboard(text) {
 function getBirthday() {
     const today = new Date();
     const year = today.getFullYear();
-    const birthday = new Date(year, 7, 31);
-    const birthdayStatic = new Date('2008-08-31');
-    
-    let oldFreak = year - birthdayStatic.getFullYear() + 1;
+    const birthday = new Date(year, 7, 31); 
+    const birthYear = 2008;
 
-    if (today < birthday) {
-        oldFreak--;
-    }
-    const daysUntil = Math.ceil((new Date(year + 1, 7, 31) - today) / (1000 * 60 * 60 * 24));
+    let age = year - birthYear;
+    if (today == birthday) age--;
 
-    switch (daysUntil) {
-        case 0 | 365:
-            return `It's my birthday! i'm ${Math.floor(oldFreak)} now`; 
-        default:
-            return `i'll be ${Math.floor(oldFreak)} in ${daysUntil} days`;
-    }
+    if (today > birthday) birthday.setFullYear(year + 1);
+
+    const daysUntil = Math.ceil((birthday - today) / (86400000)); 
+
+    return daysUntil >= 0 && daysUntil <= 365 ? `i will be ${age} in ${daysUntil} days` : `it's my birthday! i'm ${age} now`;
 }
